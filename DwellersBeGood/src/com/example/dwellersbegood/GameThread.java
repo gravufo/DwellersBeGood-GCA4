9fb1e999e2765554edb2dde0e02d55ace407f921
Yanick <Yanick@132.207.251.19>
2014-01-17 13:51:37 -0500

    Dwellers be good are about to start shits

---
+++ DwellersBeGood/src/com/example/dwellersbegood/GameThread.java
@@ -0,0 +1,64 @@
+package com.example.dwellersbegood;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread{
+	
+	private boolean m_run = false;
+    private long m_ElaspedThreadTime;
+    private long m_NewTime;
+    private long m_OldTime;
+    private GameView m_gameView;
+    
+    private SurfaceHolder m_surfaceHolder;
+    
+    public GameThread(GameView gameView, SurfaceHolder surfaceHolder){
+    	this.m_NewTime = System.currentTimeMillis();
+		this.m_OldTime = m_NewTime;
+    	this.m_gameView = gameView;
+    	this.m_surfaceHolder = surfaceHolder;
+    }
+    
+    @Override
+    public void run() {
+        Canvas c;
+        while (this.m_run) {
+            c = null;
+            try 
+            {
+	           	this.m_NewTime = System.nanoTime();
+	           	this.m_ElaspedThreadTime += this.m_NewTime - this.m_OldTime;
+
+	            c = this.m_surfaceHolder.lockCanvas(null);
+	            if ((this.m_ElaspedThreadTime/1000000000.0) >= 1/60)
+	            {
+	                	
+	            	synchronized (this.m_surfaceHolder) {
+	                		
+	                	// On dessine et on update notre panel
+	                	this.m_gameView.Update();
+	                	this.m_gameView.onDraw(c);
+	                }
+	            }
+	            this.m_OldTime = this.m_NewTime;
+            } 
+            finally 
+            {
+                // do this in a finally so that if an exception is thrown
+                // during the above, we don't leave the Surface in an
+                // inconsistent state
+                if (c != null) 
+                {
+                	this.m_surfaceHolder.unlockCanvasAndPost(c);
+                }
+            }
+        }
+    }
+    
+    public void setRunning(boolean run) {
+    	this.m_run = run;
+    }
+
+
+}
---
+++ DwellersBeGood/src/com/example/dwellersbegood/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.dwellersbegood;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	// For background music management, see :
+	//		http://www.rbgrn.net/content/307-light-racer-20-days-61-64-completion
+	private MediaPlayer m_Player;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Set the right font for title
+		TextView myTextView=(TextView)findViewById(R.id.textGameTitle);
+		Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/Typo Oxin free promo.ttf");
+		myTextView.setTypeface(typeFace);
+		
+		// Activate background music
+		this.m_Player = MediaPlayer.create(this, R.raw.radiomartini);
+		this.m_Player.setLooping(true);
+		this.m_Player.start();
+	}
+	
+	/** Called when the user clicks the Singleplayer button */
+	public void SingleplayerMode(View view){
+		Intent intent = new Intent(this, GameActivity.class);
+		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Multiplayer button */
+	public void MultiplayerMode(View view){
+		Intent intent = new Intent(this, MultiplayerConnectionActivity.class);
+		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Settings button */
+	public void Settings(View view){
+		Intent intent = new Intent(this, SettingsActivity.class);
+		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Quit button */
+	public void Quit(View view){
+		this.finish();
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		this.m_Player.pause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		this.m_Player.start();
+	}
+
+
+}
