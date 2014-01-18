37ed4d31bdd8464330508735cd9c49eb93351d3e
Christian Artin <gravufo@gmail.com>
2014-01-18 00:08:55 -0500

    Test for the gamethread timer

--- src/com/dwellersbegood/GameThread.java
+++ src/com/dwellersbegood/GameThread.java
@@ -4,64 +4,69 @@ import android.annotation.SuppressLint;
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
-public class GameThread extends Thread{
-	
+public class GameThread extends Thread
+{
+	public static final int nano = 1000000000;
 	private boolean m_run = false;
-    private long m_ElaspedThreadTime;
-    private long m_NewTime;
-    private long m_OldTime;
-    private GameView m_gameView;
-    
-    private SurfaceHolder m_surfaceHolder;
-    
-    public GameThread(GameView gameView, SurfaceHolder surfaceHolder){
-    	this.m_NewTime = System.currentTimeMillis();
+	private long m_ElaspedThreadTime;
+	private long m_NewTime;
+	private long m_OldTime;
+	private final GameView m_gameView;
+	
+	private final SurfaceHolder m_surfaceHolder;
+	
+	public GameThread(GameView gameView, SurfaceHolder surfaceHolder)
+	{
+		this.m_NewTime = System.nanoTime();
 		this.m_OldTime = m_NewTime;
-    	this.m_gameView = gameView;
-    	this.m_surfaceHolder = surfaceHolder;
-    }
-    
-    @SuppressLint("WrongCall")
+		this.m_gameView = gameView;
+		this.m_surfaceHolder = surfaceHolder;
+	}
+	
+	@SuppressLint("WrongCall")
 	@Override
-    public void run() {
-        Canvas c;
-        while (this.m_run) {
-            c = null;
-            try 
-            {
-	           	this.m_NewTime = System.currentTimeMillis();
-	           	this.m_ElaspedThreadTime = this.m_NewTime - this.m_OldTime;
-
-	            c = this.m_surfaceHolder.lockCanvas(null);
-	            //if ((this.m_ElaspedThreadTime/1000000000.0) >= 1/60)
-	            if ((this.m_ElaspedThreadTime) >= 1000/60)
-	            {
-	                	
-	            	synchronized (this.m_surfaceHolder) {
-	                		
-	                	// On dessine et on update notre panel
-	                	this.m_gameView.update(m_ElaspedThreadTime);
-	                	this.m_gameView.onDraw(c);
-	                }
-	            }
-	            this.m_OldTime = this.m_NewTime;
-            } 
-            finally 
-            {
-                // do this in a finally so that if an exception is thrown
-                // during the above, we don't leave the Surface in an
-                // inconsistent state
-                if (c != null) 
-                {
-                	this.m_surfaceHolder.unlockCanvasAndPost(c);
-                }
-            }
-        }
-    }
-    
-    public void setRunning(boolean run) {
-    	this.m_run = run;
-    }
-
-
+	public void run()
+	{
+		Canvas c;
+		while (this.m_run)
+		{
+			c = null;
+			try
+			{
+				this.m_NewTime = System.nanoTime();
+				this.m_ElaspedThreadTime = this.m_NewTime - this.m_OldTime;
+				
+				c = this.m_surfaceHolder.lockCanvas(null);
+				// if ((this.m_ElaspedThreadTime/1000000000.0) >= 1/60)
+				if ((this.m_ElaspedThreadTime) >= nano / 60)
+				{
+					
+					synchronized (this.m_surfaceHolder)
+					{
+						
+						// On dessine et on update notre panel
+						this.m_gameView.update(m_ElaspedThreadTime);
+						this.m_gameView.onDraw(c);
+					}
+				}
+				this.m_OldTime = this.m_NewTime;
+			}
+			finally
+			{
+				// do this in a finally so that if an exception is thrown
+				// during the above, we don't leave the Surface in an
+				// inconsistent state
+				if (c != null)
+				{
+					this.m_surfaceHolder.unlockCanvasAndPost(c);
+				}
+			}
+		}
+	}
+	
+	public void setRunning(boolean run)
+	{
+		this.m_run = run;
+	}
+	
 }
